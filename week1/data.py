4da4315ddba5e36e18ca9100f72cce003fecbb85
manoj <manojkumarkorada3022@gmail.com>
2025-12-14 19:54:10 +0530

    week1 program

---
+++ week1/data.py
@@ -0,0 +1,4 @@
+print(type(12))
+print(type(13.89))
+print(type("manoj"))
+print(type(True))
---
+++ week1/import.py
@@ -0,0 +1,5 @@
+import math
+print("Value of pi:", math.pi)
+print("Square root of 16:", math.sqrt(16))
+print("Cosine of 0:", math.cos(0))
+print("Sine of 0:", math.sin(0))
