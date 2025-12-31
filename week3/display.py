060c1a1095f1442aa0a9c84d46a016caf240368a
manoj <manojkumarkorada3022@gmail.com>
2025-12-31 19:59:48 +0530

    week3 programs

---
+++ week3/display.py
@@ -0,0 +1,9 @@
+a=int(input("enter first element:"))
+b=int(input("enter second element:"))
+sum=a+b
+difference=a-b
+product=a*b
+print("sum=",sum)
+print("difference=",difference)
+print("product=",product)
+
